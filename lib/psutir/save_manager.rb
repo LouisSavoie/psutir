3b749074c2b94737c9899971139c168069707eb9
Louis Savoie <thelousavoie@gmail.com>
2022-01-04 11:26:29 -0500

    add SaveManager.load

--- lib/psutir/save_manager.rb
+++ lib/psutir/save_manager.rb
@@ -5,4 +5,15 @@ module SaveManager
   def self.save(save, file = 'save.yaml')
     File.open(file, 'w') { |f| f.write(save.to_yaml) }
   end
+
+  def self.load(file = 'save.yaml')
+    begin
+      save = YAML.safe_load(File.read(file), [Symbol])
+    rescue Errno::ENOENT
+      save('error', file)
+      save = YAML.safe_load(File.read(file))
+    else
+      save
+    end
+  end
 end
--- spec/psutir/save_manager_spec.rb
+++ spec/psutir/save_manager_spec.rb
@@ -4,12 +4,21 @@ require 'yaml'
 require 'psutir/save_manager'
 
 RSpec.describe SaveManager do
-  describe '.save' do
-    let(:save) { { data: 'test' } }
 
-    it 'writes save data to YAML file' do
+  let(:save) { { data: 'test' } }
+
+  describe '.save' do
+    it 'writes save to YAML file' do
       described_class.save(save, 'test.yaml')
       expect(YAML.safe_load(File.read('test.yaml'), [Symbol])).to eq(save)
+      File.delete('test.yaml')
+    end
+  end
+
+  describe '.load' do
+    it 'loads save from YAML file' do
+      described_class.save(save, 'test.yaml')
+      expect(described_class.load('test.yaml')).to eq(save)
       File.delete('test.yaml')
     end
   end
